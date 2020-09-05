9d295fbc6053b1ed3b796fbdf1840f6190c63a56
Muhammad Harith <harith781@gmail.com>
2020-09-06 01:21:21 +0800

    create automapper instance

--- Auto Mapper/Auto Mapper/Program.cs
+++ Auto Mapper/Auto Mapper/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
+using System.Xml.Xsl;
 
 namespace Auto_Mapper
 {
@@ -6,7 +8,26 @@ namespace Auto_Mapper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Source, Destination>();
+            });
+
+            var mapper = new Mapper(configuration);
+            //OR
+            var mapper2 = configuration.CreateMapper();
+
+    
         }
+    }
+
+    public class Source
+    {
+        public int Value { get; set; }
+    }
+
+    public class Destination
+    {
+        public int Value { get; set; }
     }
 }
