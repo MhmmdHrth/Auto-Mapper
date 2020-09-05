aa6c81b5c7abd54f1c6c2290ae114202e05d5401
Muhammad Harith <harith781@gmail.com>
2020-09-06 00:55:21 +0800

    project initial

---
+++ Auto Mapper/Auto Mapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Auto_Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
