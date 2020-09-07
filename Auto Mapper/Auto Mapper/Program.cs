a03115d7389850adec3b39f4625cbb62ad09f910
Muhammad Harith <harith781@gmail.com>
2020-09-07 12:35:59 +0800

    adding auto mapper converter

--- Auto Mapper/Auto Mapper/Program.cs
+++ Auto Mapper/Auto Mapper/Program.cs
@@ -12,20 +12,34 @@ namespace Auto_Mapper
             var configuration = new MapperConfiguration(cfg =>
             {
                 cfg.CreateMap<Source, Destination>();
+                cfg.CreateMap<StringClass, IntClass>();
+
                 cfg.AddProfile<MappingProfile>();
+                cfg.CreateMap<string, int>().ConvertUsing(s => Convert.ToInt32(s));
+                cfg.CreateMap<string, DateTime>().ConvertUsing<DateTimeTypeConverter>();
             });
 
             var mapper = new Mapper(configuration);
 
-            var source2 = new Source2
+            //var source2 = new Source2
+            //{
+            //    Date = new DateTime(2018, 3, 3, 12, 45, 56)
+            //};
+
+            //var destination = mapper.Map<Destination2>(source2);
+
+            //Console.WriteLine(destination.Hour.Equals(source2.Date.Hour));
+
+            var StringClass = new StringClass
             {
-                Date = new DateTime(2018, 3, 3, 12, 45, 56)
+                Number = "5",
+                Date = "12/02/2019" //month|Day|Year
             };
 
-            var destination = mapper.Map<Destination2>(source2);
+            var IntClass = mapper.Map<IntClass>(StringClass);
 
-            Console.WriteLine(destination.Hour.Equals(source2.Date.Hour));
-
+            Console.WriteLine(IntClass.Number);
+            Console.WriteLine(IntClass.Date);
 
         }
     }
@@ -50,6 +64,26 @@ namespace Auto_Mapper
         public int Hour { get; set; }
         public int Minute { get; set; }
         public int Second { get; set; }
+    }
+
+    public class StringClass
+    {
+        public string Number { get; set; }
+        public string Date { get; set; }
+    }
+
+    public class IntClass
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public class DateTimeTypeConverter : ITypeConverter<string, DateTime>
+    {
+        public DateTime Convert(string source, DateTime destination, ResolutionContext context)
+        {
+            return System.Convert.ToDateTime(source);
+        }
     }
 
     public class MappingProfile : Profile
