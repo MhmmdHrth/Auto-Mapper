e77a44399c175912e659a8c712672aebb805aeb4
Muhammad Harith <harith781@gmail.com>
2020-09-07 11:57:34 +0800

    using profile

--- Auto Mapper/Auto Mapper/Program.cs
+++ Auto Mapper/Auto Mapper/Program.cs
@@ -12,15 +12,21 @@ namespace Auto_Mapper
             var configuration = new MapperConfiguration(cfg =>
             {
                 cfg.CreateMap<Source, Destination>();
+                cfg.AddProfile<MappingProfile>();
             });
 
             var mapper = new Mapper(configuration);
-            //OR
-            var mapper2 = configuration.CreateMapper();
 
-            var source = new Source { Value = 1 };
+            var source2 = new Source2
+            {
+                Date = new DateTime(2018, 3, 3, 12, 45, 56)
+            };
 
-            var destination = mapper.Map<Destination>(source);
+            var destination = mapper.Map<Destination2>(source2);
+
+            Console.WriteLine(destination.Hour.Equals(source2.Date.Hour));
+
+
         }
     }
 
@@ -33,4 +39,29 @@ namespace Auto_Mapper
     {
         public int Value { get; set; }
     }
+
+    public class Source2
+    {
+        public DateTime Date { get; set; }
+    }
+
+    public class Destination2
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+    }
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Source2, Destination2>()
+                .ForMember(dest => dest.Hour, option => option.MapFrom(src => src.Date.Hour))
+                .ForMember(dest => dest.Minute, option => option.MapFrom(src => src.Date.Minute))
+                .ForMember(dest => dest.Second, option => option.MapFrom(src => src.Date.Second));
+        }
+    }
+
+
 }
